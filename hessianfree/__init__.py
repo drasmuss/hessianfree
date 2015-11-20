d78fa7c5edf60347d0919de5d953ad008e89cfab
drasmuss <drasmuss@uwaterloo.ca>
2015-11-20 13:26:05 -0500

    Removed default pycuda import

--- hessianfree/__init__.py
+++ hessianfree/__init__.py
@@ -1,7 +1,17 @@
-from hessianfree import (nonlinearities, optimizers, dataplotter,
-                         loss_funcs, gpu)
+from hessianfree import nonlinearities, optimizers, dataplotter, loss_funcs
 from hessianfree import nonlinearities as nl
 from hessianfree import optimizers as opt
 from hessianfree.ffnet import FFNet
 from hessianfree.rnnet import RNNet
 from hessianfree import demos
+
+try:
+    import pycuda
+    from hessianfree import gpu
+except ImportError:
+    # going to have this silently fail here so people don't see an error
+    # message every time they import the module when they don't have pycuda
+    # installed. there is an error message provided if they actually try
+    # to run things on the GPU without pycuda installed, which should be
+    # more helpful.
+    pass
